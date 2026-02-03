ce2fb737e5a9e9957a31a93f35f3878310024cfc
ratkost00 <ratko.stikovic55@gmail.com>
2026-02-03 15:35:16 +0100

    Added adapter directory with dummy adapter code

---
+++ src/adapter/AdapterPublisher.py
@@ -0,0 +1,100 @@
+"""
+Adapter Publisher
+"""
+from threading import Condition
+import time
+
+import fastdds
+from library.Adapter import Adapter
+
+DESCRIPTION = """Adapter Publisher example for Fast DDS python bindings"""
+USAGE = ('python3 AdapterPublisher.py')
+
+class WriterListener (fastdds.DataWriterListener) :
+    def __init__(self, writer) :
+        self._writer = writer
+        super().__init__()
+
+
+    def on_publication_matched(self, datawriter, info) :
+        if (0 < info.current_count_change) :
+            print ("Publisher matched subscriber {}".format(info.last_subscription_handle))
+            self._writer._cvDiscovery.acquire()
+            self._writer._matched_reader += 1
+            self._writer._cvDiscovery.notify()
+            self._writer._cvDiscovery.release()
+        else :
+            print ("Publisher unmatched subscriber {}".format(info.last_subscription_handle))
+            self._writer._cvDiscovery.acquire()
+            self._writer._matched_reader -= 1
+            self._writer._cvDiscovery.notify()
+            self._writer._cvDiscovery.release()
+
+
+class Writer:
+
+    def __init__(self):
+        self._matched_reader = 0
+        self._cvDiscovery = Condition()
+        self.index = 0
+
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant_qos = fastdds.DomainParticipantQos()
+        factory.get_default_participant_qos(self.participant_qos)
+        self.participant = factory.create_participant(0, self.participant_qos)
+
+        self.topic_data_type = Adapter.AdapterPubSubType()
+        self.topic_data_type.set_name("Adapter")
+        self.type_support = fastdds.TypeSupport(self.topic_data_type)
+        self.participant.register_type(self.type_support)
+
+        self.topic_qos = fastdds.TopicQos()
+        self.participant.get_default_topic_qos(self.topic_qos)
+        self.topic = self.participant.create_topic("AdapterTopic", self.topic_data_type.get_name(), self.topic_qos)
+
+        self.publisher_qos = fastdds.PublisherQos()
+        self.participant.get_default_publisher_qos(self.publisher_qos)
+        self.publisher = self.participant.create_publisher(self.publisher_qos)
+
+        self.listener = WriterListener(self)
+        self.writer_qos = fastdds.DataWriterQos()
+        self.publisher.get_default_datawriter_qos(self.writer_qos)
+        self.writer = self.publisher.create_datawriter(self.topic, self.writer_qos, self.listener)
+
+
+    def write(self):
+        data = Adapter.Adapter()
+        data.message("Hello from adapter")
+        data.index(self.index)
+        self.writer.write(data)
+        print("Sending {message} : {index}".format(message=data.message(), index=data.index()))
+        self.index = self.index + 1
+
+
+    def wait_discovery(self) :
+        self._cvDiscovery.acquire()
+        print ("Writer is waiting discovery...")
+        self._cvDiscovery.wait_for(lambda : self._matched_reader != 0)
+        self._cvDiscovery.release()
+        print("Writer discovery finished...")
+
+
+    def run(self):
+        self.wait_discovery()
+        for x in range(10) :
+            time.sleep(1)
+            self.write()
+        self.delete()
+
+
+    def delete(self):
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant.delete_contained_entities()
+        factory.delete_participant(self.participant)
+
+
+if __name__ == '__main__':
+    print('Starting publisher.')
+    writer = Writer()
+    writer.run()
+    exit()
---
+++ src/adapter/AdapterSubscriber.py
@@ -0,0 +1,83 @@
+"""
+Adapter Subscriber
+"""
+import signal
+
+import fastdds
+from library.Adapter import Adapter
+
+DESCRIPTION = """Adapter Subscriber example for Fast DDS python bindings"""
+USAGE = ('python3 AdapterSubscriber.py')
+
+# To capture ctrl+C
+def signal_handler(sig, frame):
+    print('Interrupted!')
+
+class ReaderListener(fastdds.DataReaderListener):
+
+
+    def __init__(self):
+        super().__init__()
+
+
+    def on_subscription_matched(self, datareader, info) :
+        if (0 < info.current_count_change) :
+            print ("Subscriber matched publisher {}".format(info.last_publication_handle))
+        else :
+            print ("Subscriber unmatched publisher {}".format(info.last_publication_handle))
+
+
+    def on_data_available(self, reader):
+        info = fastdds.SampleInfo()
+        data = Adapter.Adapter()
+        reader.take_next_sample(data, info)
+
+        print("Received {message} : {index}".format(message=data.message(), index=data.index()))
+
+
+class Reader:
+
+
+    def __init__(self):
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant_qos = fastdds.DomainParticipantQos()
+        factory.get_default_participant_qos(self.participant_qos)
+        self.participant = factory.create_participant(0, self.participant_qos)
+
+        self.topic_data_type = Adapter.AdapterPubSubType()
+        self.topic_data_type.set_name("Adapter")
+        self.type_support = fastdds.TypeSupport(self.topic_data_type)
+        self.participant.register_type(self.type_support)
+
+        self.topic_qos = fastdds.TopicQos()
+        self.participant.get_default_topic_qos(self.topic_qos)
+        self.topic = self.participant.create_topic("AdapterTopic", self.topic_data_type.get_name(), self.topic_qos)
+
+        self.subscriber_qos = fastdds.SubscriberQos()
+        self.participant.get_default_subscriber_qos(self.subscriber_qos)
+        self.subscriber = self.participant.create_subscriber(self.subscriber_qos)
+
+        self.listener = ReaderListener()
+        self.reader_qos = fastdds.DataReaderQos()
+        self.subscriber.get_default_datareader_qos(self.reader_qos)
+        self.reader = self.subscriber.create_datareader(self.topic, self.reader_qos, self.listener)
+
+
+    def delete(self):
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant.delete_contained_entities()
+        factory.delete_participant(self.participant)
+
+
+    def run(self):
+        signal.signal(signal.SIGINT, signal_handler)
+        print('Press Ctrl+C to stop')
+        signal.pause()
+        self.delete()
+
+
+if __name__ == '__main__':
+    print('Creating subscriber.')
+    reader = Reader()
+    reader.run()
+    exit()
